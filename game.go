994de53fe3564129f8647f42738b0dad9d5f6f2f
murata <murata@kmc.gr.jp>
2017-11-25 13:21:40 +0900

    without string if le 1e15

--- game.go
+++ game.go
@@ -139,17 +139,16 @@ func str2big(s string) *big.Int {
 
 func str2bigx1000(s string) *big.Int {
 	x := new(big.Int)
-	x.SetString(s + "000", 10)
+	x.SetString(s+"000", 10)
 	return x
 }
 
 func big2exp(n *big.Int) Exponential {
-	s := n.String()
-
-	if len(s) <= 15 {
+	w := n.Bits()
+	if (len(w) == 0) || (len(w) == 1 && w[0] < 10000000000000000)) {
 		return Exponential{n.Int64(), 0}
 	}
-
+	s := n.String()
 	t, err := strconv.ParseInt(s[:15], 10, 64)
 	if err != nil {
 		log.Panic(err)
